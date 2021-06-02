51bd049b9c28774233e9ceb7966627e95d9e1ca8
ArissaraLorance <>
2021-06-02 03:15:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// FORK this starter file and save it to your own Repl.it account.
+//https://replit.com/@ArissaraLorance/Studio-Data-and-Variables-1#index.js//
+
+// Declare and initialize the 12 variables here:
+const input = require (redline-sync);
+
+let astronautCount = Number (input.question ("How many astronauts"))
+
+let date = "Monday 2019-3-18";
+let time = "10:05:34 AM";
+let astronautCount =	7 ;
+let astronautStatus	= "ready"; 
+let averageAstronautMassKg =	80.7;
+let crewMassKg =	astronautCount * averageAstronautMassKg;
+let fuelMassKg =	760000;
+let shuttleMassKg =	74842.31;
+let totalMassKg	=  crewMassKg + fuelMassKg + shuttleMassKg;
+let fuelTempCelsius	= -225;
+let fuelLevel	= 100%;
+let weatherStatus	= clear;
+
+
+let dashedline = "----------------------------------"
+
+// console.log (crewMassKg)
+console.log(dashline + "\n")+
+"> LCo4 -L"
+
+
+// Write code to generate the LC04 report here:
+
+
+
+
+// When done, have your TA check your code.
+
+
+
+
+// BONUS: Use readline-sync to prompt the user to enter the number of astronauts going on the mission.
